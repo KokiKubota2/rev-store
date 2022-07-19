ab21b04c536d1b79bef489ab7af84ccd9a8f1310
Koki Kubota <39072144+KokiKubota2@users.noreply.github.com>
2022-07-20 04:11:09 +0900

    Delete init_firebase.js

--- src/init_firebase.js
+++
@@ -1,9 +0,0 @@
-const _ = require('lodash')
-const { getApps, initializeApp } = require('firebase/app')
-
-module.exports = (firebaseConfig) => {
-  let app
-  if (_.isEmpty(getApps())) app = initializeApp(firebaseConfig)
-
-  return app
-}
