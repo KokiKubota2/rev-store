dc28853c2c4e9ddea05f4596d697a2fad317307a
Koki Kubota <39072144+KokiKubota2@users.noreply.github.com>
2022-07-21 18:18:01 +0900

    bucketName 追加

--- src/download.js
+++ src/download.js
@@ -2,7 +2,7 @@ const axios = require('axios')
 
 const verify = require('./verify')
 
-module.exports = async (jwtStr, endpoint) => {
+module.exports = async (jwtStr, bucketName, endpoint) => {
   if (!jwtStr) throw new Error('jwtStr is invalid')
   if (!endpoint) throw new Error('endpoint is invalid')
 
@@ -11,8 +11,9 @@ module.exports = async (jwtStr, endpoint) => {
   if (_.isNull(metadata)) throw new Error('invalid signature')
 
   const res = await axios.post(`${endpoint}/api/rev-store/download`, {
+    bucketName,
     path: metadata.path,
   })
 
-  return res.data.downloadUrl
+  return res.data.url
 }
